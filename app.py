08b449ddebc77da3ae53e3bb5353bbbb7b629bed
Aidas Gaučas <aidasgau2@gmail.com>
2023-08-29 19:26:04 +0300

    Update .gitignore and template files

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("home.html")
+    image_list = [
+        "/static/dummy_images/sunset_serenade_beachside_captures.jpg",
+        "/static/dummy_images/vibrant_abstract_watercolor_flow.jpg",
+        "/static/dummy_images/vibrant_glow_technology_connection_joyful_smile.jpg"
+    ]
+    return render_template('home.html', image_list=image_list)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
