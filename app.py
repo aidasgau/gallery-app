c373cd5a18c1d47b71db1967951774cff3f4437d
Aidas Gaučas <aidasgau2@gmail.com>
2023-08-30 18:58:39 +0300

    Added some more styles and made HTML changes

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
-import datetime
+from datetime import datetime 
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    image_list = [
-        "/static/dummy_images/sunset_serenade_beachside_captures.jpg",
-        "/static/dummy_images/vibrant_abstract_watercolor_flow.jpg",
-        "/static/dummy_images/vibrant_glow_technology_connection_joyful_smile.jpg"
-    ]
-    return render_template('home.html', image_list=image_list)
+    image_thumbnail_directory = os.path.join(app.static_folder, 'dummy_images', 'thumbnails')
+
+    image_files = [f for f in os.listdir(image_thumbnail_directory) if f.endswith('.png')]
+
+    image_list = [os.path.join('/static/dummy_images/thumbnails', image) for image in image_files]
+
+    current_year = datetime.now().year
+    full_name = 'Aidas Gaučas'
+
+    return render_template('home.html', image_list=image_list, current_year=current_year, full_name=full_name)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
